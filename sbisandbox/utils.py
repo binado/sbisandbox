6c1e4752d54bca567bf1814872c8d89144dcc9c7
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-05-22 14:34:02 -0300

    Add utility methods

---
+++ sbisandbox/utils.py
@@ -0,0 +1,16 @@
+from sbi.utils.user_input_checks import (
+    check_sbi_inputs,
+    process_prior,
+    process_simulator,
+)
+
+def validate_model(prior, simulator):
+    # Check prior, return PyTorch prior.
+    _prior, num_parameters, prior_returns_numpy = process_prior(prior)
+
+    # Check simulator, returns PyTorch simulator able to simulate batches.
+    _simulator = process_simulator(simulator, _prior, prior_returns_numpy)
+
+    # Consistency check after making ready for sbi.
+    check_sbi_inputs(_simulator, _prior)
+    return _prior, _simulator

a66c10038d5fc0a6d4ac7dce0c6c603d5359748d
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-06-10 17:31:21 -0300

    Reformat imports in remaining examples

--- sbisandbox/examples/gaussian_linear.py
+++ sbisandbox/examples/gaussian_linear.py
@@ -1,7 +1,8 @@
 import torch
 from torch.distributions import MultivariateNormal
 
-from .toymodel import ToyModel, MultivariateNormalMixin
+from ..toymodel import ToyModel, MultivariateNormalMixin
+
 
 class GaussianLinearToyModel(MultivariateNormalMixin, ToyModel):
     def __init__(self, nparams=10, ndata=10):
@@ -13,10 +14,12 @@ class GaussianLinearToyModel(MultivariateNormalMixin, ToyModel):
 
     @property
     def prior(self):
-        return MultivariateNormal(self.prior_loc, covariance_matrix=self.prior_covariance_matrix)
-    
+        return MultivariateNormal(
+            self.prior_loc, covariance_matrix=self.prior_covariance_matrix
+        )
+
     def loc(self, params: torch.Tensor) -> torch.Tensor:
         return params
-    
+
     def covariance_matrix(self, params: torch.Tensor) -> torch.Tensor:
         return 0.1 * torch.eye(params.shape)
--- sbisandbox/examples/slcp.py
+++ sbisandbox/examples/slcp.py
@@ -1,6 +1,7 @@
 import torch
 
-from .toymodel import ToyModel, UniformPriorMixin, MultivariateNormalMixin
+from ..toymodel import ToyModel, UniformPriorMixin, MultivariateNormalMixin
+
 
 class SLCPToyModel(UniformPriorMixin, MultivariateNormalMixin, ToyModel):
     def __init__(self) -> None:
@@ -9,19 +10,16 @@ class SLCPToyModel(UniformPriorMixin, MultivariateNormalMixin, ToyModel):
     @property
     def low(self) -> torch.Tensor:
         return -1 * torch.ones(self.params_dimensionality)
-    
+
     @property
     def high(self) -> torch.Tensor:
         return torch.ones(self.params_dimensionality)
-    
+
     def loc(self, params: torch.Tensor) -> torch.Tensor:
         return params[:2]
-    
+
     def covariance_matrix(self, params: torch.Tensor) -> torch.Tensor:
-        s1  = params[2].item() ** 2
-        s2 =  params[3].item() ** 2
+        s1 = params[2].item() ** 2
+        s2 = params[3].item() ** 2
         rho = torch.atan(params[-1]).item()
-        return torch.tensor([
-            [s1 ** 2, rho * s1 * s2],
-            [rho * s1 * s2, s2 ** 2]
-        ])
+        return torch.tensor([[s1**2, rho * s1 * s2], [rho * s1 * s2, s2**2]])

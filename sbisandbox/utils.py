3f38673ce0e785fcdb9a322a4d5a529b94580949
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-06-16 02:38:58 -0300

    Add tensor_to_dataset util method

--- sbisandbox/utils.py
+++ sbisandbox/utils.py
@@ -1,8 +1,20 @@
+from typing import Sequence
+
+from torch import Tensor
+
 from sbi.utils.user_input_checks import (
     check_sbi_inputs,
     process_prior,
     process_simulator,
 )
+
+
+def tensor_to_dataset(labels: Sequence[str], tensor: Tensor) -> dict[str, Tensor]:
+    ndim = tensor.shape[-1]
+    assert (
+        len(labels) == ndim
+    ), "Number of labels does not correspond to tensor event_dim"
+    return dict(zip(labels, (tensor[..., i] for i in range(ndim))))
 
 
 def if_none_else(a, b):

17011444a2ba9bc7cccaf27b6e813359066533d4
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-06-17 23:42:52 -0300

    Update ToyModel base class

--- sbisandbox/toymodel.py
+++ sbisandbox/toymodel.py
@@ -1,42 +1,75 @@
 from abc import ABC, abstractmethod
 
 import torch
-from torch.distributions import Uniform, Independent, MultivariateNormal, Distribution
+from torch import Tensor
+from torch.distributions import MultivariateNormal, Distribution
+import pyro
+import pyro.distributions as dist
+from sbi.inference import simulate_for_sbi
+
+from .utils import validate_model
 
 
 class ToyModel(ABC):
-    def __init__(self, ndim) -> None:
-        self.ndim = torch.tensor(ndim, dtype=int)
+    def __init__(
+        self, theta_event_shape: torch.Size, x_event_shape: torch.Size
+    ) -> None:
+        self.theta_event_shape = theta_event_shape
+        self.x_event_shape = x_event_shape
+        self.labels = map(
+            lambda n: f"theta_{n}", range(1, self.params_dimensionality + 1)
+        )
+        self._plates = []
         super().__init__()
 
     @property
     def params_dimensionality(self):
-        return self.ndim[0]
+        return self.theta_event_shape[0]
 
     @property
     def data_dimensionality(self):
-        return self.ndim[1]
-
-    @property
-    @abstractmethod
-    def x0(self) -> torch.Tensor:
-        raise NotImplementedError
+        return self.x_event_shape
 
     @property
     @abstractmethod
     def prior(self) -> Distribution:
         raise NotImplementedError
 
-    @abstractmethod
-    def simulator(self, params: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
+    def _prior_model(self):
+        return pyro.sample("theta", self.prior)
 
     @abstractmethod
-    def loglike(self, params: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
+    def _pyro_model(self):
         raise NotImplementedError
 
-    def log_prob(self, params: torch.Tensor, x: torch.Tensor):
+    def _simulator_from_pyro_model(self, theta: Tensor):
+        _data = {"theta": theta}
+        return pyro.poutine.condition(self._pyro_model, data=_data)
+
+    def pyro_model_to_mcmc(self, x: Tensor):
+        _data = {"x": x}
+        return pyro.poutine.condition(self._pyro_model, data=_data)
+
+    def simulator(self, theta: Tensor) -> Tensor:
+        @pyro.poutine.condition(data={"theta": theta})
+        def _simulator():
+            with pyro.plate("data", theta.shape[0]):
+                return self._pyro_model()
+
+        return _simulator()
+
+    def loglike(self, params: Tensor, x: Tensor) -> Tensor:
+        raise NotImplementedError
+
+    def log_prob(self, params: Tensor, x: Tensor):
         return self.loglike(params, x) + self.prior.log_prob(params)
+
+    def get_observations(self, num_observations: int, seed: int):
+        _prior, _simulator = validate_model(self.prior, self.simulator)
+        theta, x = simulate_for_sbi(
+            _simulator, proposal=_prior, num_simulations=num_observations, seed=seed
+        )
+        return theta, x
 
 
 class UniformPriorMixin:
@@ -50,28 +83,34 @@ class UniformPriorMixin:
     def high(self):
         raise NotImplementedError
 
+    def _prior_dist(self):
+        return dist.Uniform(low=self.low, high=self.high).to_event(1)
+
     @property
-    def prior(self) -> Distribution:
-        dist = Uniform(low=self.low, high=self.high)
-        return Independent(dist, 1)
+    def prior(self):
+        return self._prior_dist()
 
 
 class MultivariateNormalMixin:
+    theta_independtly_sampled = False
+
     @abstractmethod
-    def loc(self, params: torch.Tensor) -> torch.Tensor:
+    def loc(self, theta: Tensor) -> Tensor:
         raise NotImplementedError
 
     @abstractmethod
-    def covariance_matrix(self, params: torch.Tensor) -> torch.Tensor:
+    def covariance_matrix(self, theta: Tensor) -> Tensor:
         raise NotImplementedError
 
-    def _dist(self, params: torch.Tensor) -> MultivariateNormal:
-        loc = self.loc(params)
-        covariance_matrix = self.covariance_matrix(params)
-        return MultivariateNormal(loc, covariance_matrix=covariance_matrix)
+    def _dist(self, theta: Tensor) -> MultivariateNormal:
+        loc = self.loc(theta)
+        covariance_matrix = self.covariance_matrix(theta)
+        return dist.MultivariateNormal(loc, covariance_matrix=covariance_matrix)
 
-    def simulator(self, params: torch.Tensor) -> torch.Tensor:
-        return self._dist(params).sample()
+    def _pyro_model(self):
+        theta = self._prior_model()
+        _dist = self._dist(theta)
+        return pyro.sample("x", _dist)
 
-    def loglike(self, params: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
-        return self._dist(params).log_prob(x)
+    def loglike(self, theta: Tensor, x: Tensor) -> Tensor:
+        return self._dist(theta).log_prob(x)

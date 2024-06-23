07f5c518c65bddd7fe0672ad2a0fa91cbff6d9b7
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-06-23 17:48:13 -0300

    Add GaussianMixture toy model

--- sbisandbox/examples/__init__.py
+++ sbisandbox/examples/__init__.py
@@ -1,7 +1,13 @@
 # ruff: noqa: F401
 from .gaussian_linear import GaussianLinearToyModel
 from .gaussian_linear_uniform import GaussianLinearUniformToyModel
+from .gaussian_mixture import GaussianMixtureToyModel
 from .twomoons import TwoMoonsToyModel
 from .slcp import SLCPToyModel
 
-__models__ = ("Gaussian Linear", "Gaussian Linear Uniform", "Two moons", "SLCP")
+__models__ = (
+    "Gaussian Linear",
+    "Gaussian Linear Uniform",
+    "Gaussian Mixture" "Two Moons",
+    "SLCP",
+)

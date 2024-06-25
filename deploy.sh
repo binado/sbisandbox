02d476ef8268c12b8550166bef69289c0ea2dabb
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-06-25 01:46:25 -0300

    Deployed 85eeeda with MkDocs version: 1.6.0

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 jupyter nbconvert notebooks/introduction.ipynb --to markdown --output-dir docs/tutorials
+jupyter nbconvert notebooks/twomoons.ipynb --to markdown --output-dir docs/tutorials
 mkdocs gh-deploy
 rm -r docs/tutorials/introduction*
+rm -r docs/tutorials/twomoons*

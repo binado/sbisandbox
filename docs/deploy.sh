417198117070e4e9a34fbb3ba1f2385604475962
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-07-02 09:46:42 -0300

    Update docs

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,5 +1,7 @@
 jupyter nbconvert notebooks/introduction.ipynb --to markdown --output-dir docs/tutorials
 jupyter nbconvert notebooks/twomoons.ipynb --to markdown --output-dir docs/tutorials
+jupyter nbconvert notebooks/mcmc.ipynb --to markdown --output-dir docs/tutorials
 mkdocs gh-deploy
 rm -r docs/tutorials/introduction*
 rm -r docs/tutorials/twomoons*
+rm -r docs/tutorials/mcmc*
